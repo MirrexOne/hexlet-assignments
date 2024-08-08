728227f1da9848d43b690302d4a91984b3aa3361
@hexlet/cli <support@hexlet.io>
2024-08-08 23:15:00 +0300

    submit java-spring-ru/annotations

---
+++ java-spring-ru/annotations/src/main/java/exercise/Application.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import exercise.model.Address;
+import exercise.annotation.Inspect;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Application {
+    public static void main(String[] args) {
+        var address = new Address("London", 12345678);
+
+        // BEGIN
+        for (Method method : Address.class.getDeclaredMethods()) {
+
+            if (method.isAnnotationPresent(Inspect.class)) {
+
+                System.out.println("Method " + method.getName() + " returns a value of type " + method.getReturnType());
+
+                try {
+                    method.invoke(address);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        // END
+    }
+}
